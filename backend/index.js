3d1879ca128d65647d72d14063389410d211ce3d
Young Yu <youngyu19@gmail.com>
2023-12-15 12:26:23 -0500

    add dependencies for backend server

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+/* Entry point for backend server */
+const mongoose = require("mongoose");
+const axios = require("axios");
+const express = require("express");
+const dotenv = require("dotenv");
+
+/* Create instance of backend server */
+const app = express();
+app.use(express.json());
+
+/* Load .env file */
+dotenv.config();
+
