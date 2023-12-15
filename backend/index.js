82dcbce7854ae0053c92bf9ff4057604d1b00efe
Young Yu <youngyu19@gmail.com>
2023-12-15 12:44:42 -0500

    fix eslint issues

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,12 @@
 /* Entry point for backend server */
-const mongoose = require("mongoose");
-const axios = require("axios");
-const express = require("express");
-const dotenv = require("dotenv");
+// const mongoose = require('mongoose');
+// const axios = require("axios");
+const express = require('express')
+const dotenv = require('dotenv')
 
 /* Create instance of backend server */
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
 /* Load .env file */
-dotenv.config();
-
+dotenv.config()
