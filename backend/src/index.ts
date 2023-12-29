6a292b0b1400063cd03752653f153af79c856528
Young Yu <youngyu19@gmail.com>
2023-12-28 17:19:39 -0800

    make unit tests for app.ts and add retries to mongo (#46)
    * make unit tests for app.ts and add retries to mongo
    
    * fix lint issues

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,6 +14,25 @@ if (!PORT) {
   process.exit(1); // TODO: add errors for this, catch, and handle it
 }
 
-/* Create instance of backend app server and listen for connections*/
-const appInstance = new App();
-appInstance.startServer(parseInt(PORT));
+/* Start application */
+startApp(PORT);
+
+/**
+ * Initializes middlewares, mounts API routes, connects to MongoDB, and starts the backend server.
+ */
+async function startApp(port: string) {
+  const appInstance = new App();
+  appInstance.initializeMiddleWares();
+  appInstance.mountRoutes();
+
+  try {
+    /* Wait for the MongoDB connection to be established */
+    await appInstance.connectToDatabase();
+
+    /* Start the server after successful database connection */
+    await appInstance.startServer(parseInt(port));
+  } catch (error) {
+    console.error('Failed to start the application:', error);
+    process.exit(1);
+  }
+}
