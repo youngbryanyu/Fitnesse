9ca75462a3f44ab7ddde03bbef9f575386a7facc
Young Yu <youngyu19@gmail.com>
2024-01-08 18:34:58 -0800

    finish login implementation

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -4,8 +4,11 @@ import { UserModel } from '../models/userModel';
 import CryptoJS from 'crypto-js';
 import { AUTH_RESPONSES, PASSWORD_RULES } from '../config/constants';
 import { GENERIC_RESPONSES } from '../config/constants';
-import AppConfig from '../config/appConfig';
+import EnvConfig from '../config/envConfig';
 import logger from '../logging/logger';
+import { FailedLoginUserModel } from '../models/failedLoginUserModel';
+import { LockedOutUserModel } from '../models/lockedOutUserModel';
+import jwt from 'jsonwebtoken';
 
 /**
  * Controller that contains authentication business logic
@@ -36,7 +39,7 @@ class AuthController {
         username: req.body.username
       });
       if (existingUsernameUser) {
-        logger.info(`The username ${req.body.username} is already taken.`);
+        logger.info(`The username [${req.body.username}] is already taken.`);
         res.status(409).json({
           message: AUTH_RESPONSES._409_USERNAME_TAKEN
         });
@@ -48,7 +51,7 @@ class AuthController {
         email: req.body.email
       });
       if (existingEmailUser) {
-        logger.info(`The email ${req.body.email} is already taken.`);
+        logger.info(`The email [${req.body.email}] is already taken.`);
         res.status(409).json({
           message: AUTH_RESPONSES._409_EMAIL_TAKEN
         });
@@ -57,7 +60,7 @@ class AuthController {
 
       /* Password validation. This should be validated on the front end first */
       if (AuthController.isValidPassword(req.body.password) === false) {
-        logger.info(`The password is invalid.`);
+        logger.info(`The password is invalid and doesn't satisfy the requirements.`);
         res.status(422).json({
           message: AUTH_RESPONSES._422_INVALID_PASSWORD
         });
@@ -65,8 +68,8 @@ class AuthController {
       }
 
       /* Get secret key from environment variables for password encryption */
-      const appConfig = new AppConfig();
-      const secretKey = appConfig.getConfig('AES_SECRET_KEY');
+      const envConfig = new EnvConfig();
+      const secretKey = envConfig.getConfigString('SECRET_KEY');
 
       /* Create a new user */
       const newUser = new UserModel({
@@ -78,6 +81,7 @@ class AuthController {
 
       /* Try saving new user to database */
       const savedUser = await newUser.save();
+      logger.info(`Register successful for user [${savedUser._id}]`);
       res.status(201).json({
         message: AUTH_RESPONSES._201_REGISTER_SUCCESSFUL,
         user: savedUser
@@ -100,6 +104,153 @@ class AuthController {
     }
 
     return true;
+  }
+
+  /**
+   * Attempts to login. 
+   * 
+   * Expected body:
+   * {
+   *    emailOrUsername: ...
+   *    password: ...
+   * }
+   * 
+   * Response:
+   * {
+   *    message: ...,
+   *    info: ...
+   * }
+   * @param req incoming request from client.
+   * @param res response to return to client.
+   */
+  static async login(req: Request, res: Response): Promise<void> {
+    try {
+      /* Try to find a user that matches the email or username */
+      const user = await UserModel.findOne({
+        $or: [
+          { email: req.body.emailOrUsername },
+          { username: req.body.emailOrUsername }
+        ]
+      });
+
+      /* Invalid credentials - email or username doesn't exist */
+      if (!user) {
+        logger.info(`Username or email [${req.body.emailOrUsername}] isn't recognized.`);
+        res.status(401).json({
+          message: AUTH_RESPONSES._401_INVALID_CREDENTIALS
+        });
+        return;
+      }
+      
+      /* Check if user is locked out already */
+      const lockedOutUser = await LockedOutUserModel.findOne({
+        userId: user._id
+      });
+      if (lockedOutUser) {
+        logger.info(`User [${user._id}] is currently locked out due to too many failed login attempts.`);
+        res.status(429).json({
+          message: AUTH_RESPONSES._429_TOO_MANY_FAILED_LOGINS
+        });
+        return;
+      }
+      
+      /* Create app config instance */
+      const appConfig = new EnvConfig();
+      
+      /* Get AES secret key from environment variables */
+      const secretKey = appConfig.getConfigString('SECRET_KEY');
+      
+      /* Check if password matches the decrypted version of the encrypted password stored in the database */
+      const originalPasswordBytes = CryptoJS.AES.decrypt(user.password, secretKey);
+      const originalPassword = originalPasswordBytes.toString(CryptoJS.enc.Utf8);
+      if (originalPassword !== req.body.password) {
+        /* Check if user already has recently failed login */
+        const failedLoginUser = await FailedLoginUserModel.findOne({
+          userId: user._id
+        });
+        if (!failedLoginUser) {
+          /* Create new entry for user who failed to login recently */
+          const newFailedLoginUser = new FailedLoginUserModel({
+            userId: user._id,
+            numFailed: 1,
+            createdAt: Date.now()
+          });
+          await newFailedLoginUser.save();
+          logger.info(`Added user [${user._id}] to recently failed login collection.`);
+        } else {
+          /* Update existing entry for user who failed to login recently */
+          failedLoginUser.numFailed++;
+          
+          /* Check if the number of times recently failed exceeds the threshold for the user */
+          const maxFailedLogins = appConfig.getConfigNumber('AUTH.MAX_FAILED_LOGINS')
+          if (failedLoginUser.numFailed >= maxFailedLogins) {
+            /* Add user to locked out collection */
+            const lockedOutUser = new LockedOutUserModel({
+              userId: user._id,
+              createdAt: Date.now()
+            });
+            await lockedOutUser.save();
+            logger.info(`Added user [${user._id}] to locked out users collection.`);
+
+            /* Remove user from recently failed login collection */
+            await failedLoginUser.deleteOne();
+            logger.info(`Removed user [${user._id}] from recently failed login collection.`);
+          } else {
+            /* Save the incremented entry for recently failed login user */
+            await failedLoginUser.save();
+            logger.info(`Incremented count of user [${user._id}] in recently failed login collection.`);
+          }
+        }
+
+        /* Send invalid credentials response to client */
+        logger.info(`Login for user [${user._id}] failed due to invalid password.`);
+        res.status(401).json({
+          message: AUTH_RESPONSES._401_INVALID_CREDENTIALS
+        });
+        return;
+      }
+
+      /* Delete user from locked out table and recently failed logins table upon successful login */
+      await FailedLoginUserModel.findOneAndDelete({
+        userId: user._id
+      });
+      await LockedOutUserModel.findOneAndDelete({
+        userId: user._id
+      });
+      logger.info(`Removed user [${user._id}] from recently failed login collection and locked out collection.`);
+
+      /* Sign access token and refresh tokens */
+      const accessTokenLifetime = appConfig.getConfigString('AUTH.ACCESS_TOKEN_LIFETIME');
+      const accessToken = jwt.sign(
+        { id: user._id },
+        secretKey,
+        { expiresIn: accessTokenLifetime }
+      );
+      const refreshTokenLifetime = appConfig.getConfigString('AUTH.REFRESH_TOKEN_LIFETIME');
+      const refreshToken = jwt.sign(
+        { id: user._id },
+        secretKey,
+        { expiresIn: refreshTokenLifetime }
+      );
+
+      /* Return user info and access + refresh tokens to client */
+      // eslint-disable-next-line
+      const { password, ...info } = user._doc;
+      logger.info(`Login successful for user [${user._id}].`);
+      res.status(200).json({
+        message: AUTH_RESPONSES._200_LOGIN_SUCCESSFUL,
+        info: {
+          ...info,
+          accessToken: accessToken,
+          refreshToken: refreshToken
+        }
+      });
+    } catch (error) {
+      logger.error("Server error occured: " + error);
+      res.status(500).json({
+        message: GENERIC_RESPONSES[500]
+      });
+    }
   }
 }
 
--- backend/tests/routes/authRoutes.test.ts
+++ backend/tests/routes/authRoutes.test.ts
@@ -3,11 +3,14 @@ import request from 'supertest';
 import AuthController from '../../src/controllers/authController';
 import { API_URLS_V1, AUTH_RESPONSES } from '../../src/config/constants';
 import App from '../../src/app';
-import AppConfig from '../../src/config/appConfig';
+import EnvConfig from '../../src/config/envConfig';
 
 /* Mock the register endpoint to do nothing */
 jest.mock('../../src/controllers/authController', () => ({
   register: jest.fn().mockImplementation((req, res) => {
+    res.sendStatus(201); // Mock implementation
+  }),
+  login: jest.fn().mockImplementation((req, res) => {
     res.sendStatus(201); // Mock implementation
   }),
 }));
@@ -16,16 +19,18 @@ jest.mock('../../src/controllers/authController', () => ({
 describe('Auth Routes Tests', () => {
   let appInstance: App;
 
-  /* Initialize app instance before all tests */
+  /* Setup before all tests */
   beforeAll(() => {
+    /* Initialize app instance and call setup functions before all tests */
     appInstance = new App();
     appInstance.initializeMiddleWares();
     appInstance.mountRoutes();
   });
 
-  /* Clear all mocks before each test */
+  /* Setup before each test */
   beforeEach(() => {
-    jest.clearAllMocks();
+    /* Clear mock states but not their implementation */
+    jest.clearAllMocks(); 
   });
 
   /* Test that the register controller function is called on POST /register */
@@ -40,7 +45,7 @@ describe('Auth Routes Tests', () => {
   /* Test when the rate limit is exceeded for registering */
   it('should response with 429 when the rate limit is exceeded for POST /register', async () => {    
     /* Get register threshold from test .env file */
-    const appConfig = new AppConfig();
+    const appConfig = new EnvConfig();
     const threshold = appConfig.getConfigNumber('RATE_LIMITING.AUTH.REGISTER.THRESHOLD');
 
     /* Call API `threshold` times so that next call will cause rating limiting */
@@ -53,6 +58,35 @@ describe('Auth Routes Tests', () => {
 
     /* Make checks */
     expect(AuthController.register).toHaveBeenCalled();
+    expect(response.statusCode).toBe(429);
+    expect(response.body.message).toBe(AUTH_RESPONSES._429_RATE_LIMIT_EXCEEDED);
+  });
+
+  /* Test that the login controller function is called on POST /login */
+  it('should call AuthController.register on POST /register', async () => {    
+    /* Make the API call */
+    await request(appInstance.express).post(`${API_URLS_V1.AUTH}/login`).send({});
+
+    /* Check that the register controller was called*/
+    expect(AuthController.login).toHaveBeenCalled();
+  });
+
+  /* Test when the rate limit is exceeded for registering */
+  it('should response with 429 when the rate limit is exceeded for POST /login', async () => {    
+    /* Get register threshold from test .env file */
+    const appConfig = new EnvConfig();
+    const threshold = appConfig.getConfigNumber('RATE_LIMITING.AUTH.LOGIN.THRESHOLD');
+
+    /* Call API `threshold` times so that next call will cause rating limiting */
+    for (let i = 0; i < threshold; i++) {
+      await request(appInstance.express).post(`${API_URLS_V1.AUTH}/login`).send({});
+    }
+
+    /* Call API to trigger rate limiting */
+    const response = await request(appInstance.express).post(`${API_URLS_V1.AUTH}/login`).send({});
+
+    /* Make checks */
+    expect(AuthController.login).toHaveBeenCalled();
     expect(response.statusCode).toBe(429);
     expect(response.body.message).toBe(AUTH_RESPONSES._429_RATE_LIMIT_EXCEEDED);
   });
