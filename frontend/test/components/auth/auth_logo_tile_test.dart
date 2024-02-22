6c44e8664a79a733f5368d1713eeeb78420520b4
Young Yu <youngyu19@gmail.com>
2024-02-22 00:22:27 -0800

    fix

--- frontend/test/components/auth/auth_logo_tile_test.dart
+++ frontend/test/components/auth/auth_logo_tile_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend/components/auth/auth_logo_tile.dart';
